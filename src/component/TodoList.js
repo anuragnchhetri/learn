55e9610221d5b2d9e3e65357a20d77cbb7ef660e
anuragnchhetri <85178620+anuragnchhetri@users.noreply.github.com>
2023-10-07 21:13:51 +0530

    commit code

---
+++ src/component/TodoList.js
@@ -0,0 +1,45 @@
+
+import React ,{useState}from 'react'
+
+const TodoList = () => {
+    
+const [todos, setTodos]= useState([]);
+const [newTodo, setNewTodo] = useState("");
+
+const handleAddTodo = ()=>{
+    if(newTodo.trim("")!==""){
+        setTodos([...todos,{text: newTodo.trim(),checked: false}])
+        setNewTodo("");
+    }
+}
+const handleDeleteTodo =(index)=>{
+    const newTodos=[...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+};
+const handleToggleTodo=(index)=>{
+    const newTodos=[...todos];
+    newTodos[index].checked=! newTodos[index].checked;
+    setTodos(newTodos)
+};
+  return (
+    <div className="main">
+    <h1>To-do List</h1>
+    <input type='text' value={newTodo} onChange={(e)=>{setNewTodo(e.target.value)}}/>
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+          {todos.map((todo,index)=>{
+              <li key={index}>
+                  <input type="checkbox" checked={todos.checked} onChange={()=>handleToggleTodo(index)} />
+              <span>{todo.text}</span>
+              <button onClick={()=>handleDeleteTodo(index)}>delete</button>
+              </li>
+          })} 
+      </ul>
+  </div>
+  )
+}
+
+export default TodoList;
+
+
